98b6fb13a9f737c205fbd2894a68a04afb28c27b
Ryuuji73Takasu <mrh.sourav54@gmail.com>
2021-05-25 15:12:05 +0600

    Login Completed

---
+++ src/components/LoginForm.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+
+function LoginForm({ Login, error }) {
+  const [details, setDetails] = useState({
+    name: "",
+    email: "",
+    passwword: "",
+  });
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    Login(details);
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className='form-inner'>
+        <h2>Login</h2>
+        {error !== "" ? <div className='error'>{error}</div> : ""}
+        <div className='form-group'>
+          <label htmlFor='name'>Name</label>
+          <input
+            type='text'
+            name='name'
+            id='name'
+            onChange={(e) => setDetails({ ...details, name: e.target.value })}
+            value={details.name}
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='email'>Email</label>
+          <input
+            type='email'
+            name='email'
+            id='email'
+            onChange={(e) => setDetails({ ...details, email: e.target.value })}
+            value={details.email}
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='password'>Password</label>
+          <input
+            type='password'
+            name='password'
+            id='password'
+            onChange={(e) =>
+              setDetails({ ...details, password: e.target.value })
+            }
+            value={details.password}
+          />
+        </div>
+        <div style={{ height: 20 }}></div>
+        <input type='submit' value='LOGIN' />
+      </div>
+    </form>
+  );
+}
+
+export default LoginForm;
